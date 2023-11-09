238c7bf96e56cb6b82cc7672575a766b09c0f7e9
coldok <2coldok@gmail.com>
2023-11-09 20:49:24 +0900

    feat(Event) : 주어진 날짜가 속하는 특정 기간에 대해 알려주는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,16 +3,3 @@ class App {
 }
 
 export default App;
-
-
-
-function getWeekDay(date) {
-  let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-
-  return days[date.getDay()];
-}
-
-let date = new Date(2023, 10, 9);
-
-console.log(getWeekDay(date));
-
---
+++ src/domain/Event.js
@@ -0,0 +1,48 @@
+
+// number
+
+class Event {
+  #days;
+
+  #date;
+
+  constructor(days) {
+    this.#days = days;
+    this.#date = new Date(2023, 11, days);
+  }
+  // 크리스마스 디데이 기간 (1~25일)
+  christmasPeriod() {
+    if (this.#days <= 25) {
+      return true;
+    }
+    return false
+  }
+  
+  // 주말 (금, 토)
+  weekend() {
+    if (this.#getWeek() === 'FR' || this.#getWeek() === 'SA') {
+      return true;
+    }
+    return false
+  }
+  
+  // 일요일, 크리스마스(25)
+  specialPeriod() {
+    if (this.#getWeek() === 'SU' || this.#days === 25) {
+      return true;
+    }
+    return false
+  }
+
+  #getWeek() {
+    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
+    return days[this.#date.getDay()];
+  }
+
+  reservationDate() {
+    return this.#days;
+  }
+}
+
+export default Event;
