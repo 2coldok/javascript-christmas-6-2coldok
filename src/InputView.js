b569ba5cf702875cae14486204196db449c5a144
coldok <2coldok@gmail.com>
2023-11-11 01:36:22 +0900

    feat : InputView OutputVieew 구현 완료

--- src/InputView.js
+++ src/InputView.js
@@ -1,9 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import { QUESTION } from "./constants/PlannerMesseage.js";
+
 const InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+  async readDate() {
+    return await Console.readLineAsync(QUESTION.date);
+  },
+    
+  async readMenu() {
+    return await Console.readLineAsync(QUESTION.menu);
+  },
+};
 
 export default InputView;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,42 @@
+import { Console } from "@woowacourse/mission-utils";
+import { ANSWER } from "./constants/PlannerMesseage.js";
+
 const OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+  printIntroduce() {
+    Console.print(ANSWER.introduce);
+  },
+  
+  printProlog(date) {
+    Console.print(ANSWER.prolog(date));
+  },
+
+  printOrderMenu() {
+    Console.print(ANSWER.orderMenu);       
+  },
+
+  printTotalOrderAmount() {
+    Console.print(ANSWER.totalOrderAmount);
+  },
+
+  printFreebieMenu() {
+    Console.print(ANSWER.freebieMenu);
+  },
+
+  printBenefitList() {
+    Console.print(ANSWER.benefitList);
+  },
+
+  printTotalBenefitAmount() {
+    Console.print(ANSWER.totalBenefitAmount);
+  },
+
+  printFinalPaymentAmount() {
+    Console.print(ANSWER.finalPaymentAmount);
+  },
+
+  printBadge() {
+    Console.print(ANSWER.DecemberEventBadge);
+  },    
+};
 
 export default OutputView;
