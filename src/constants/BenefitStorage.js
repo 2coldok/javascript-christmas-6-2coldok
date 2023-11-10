fcf99973e97a7295a0b34c02c5189dc1832cca16
coldok <2coldok@gmail.com>
2023-11-10 13:52:34 +0900

    feat(EventCounter) : 구입한 음식에 해당하는 모든 혜택을 계산해주는 기능 구현

---
+++ src/constants/BenefitStorage.js
@@ -0,0 +1,19 @@
+export const DISCOUNT = Object.freeze({
+  christmasBasic : 1000,
+  christmasAccumulation : 100,
+  weekday : 2023,
+  weekend : 2023,
+  special : 1000,
+  freebieItem : '샴페인 1개',
+  freebieStandard : 120000,
+  freebieAmount : 25000,  
+});
+
+export const BADGE = Object.freeze({
+  star : 5000,
+  tree : 10000,
+  santa : 20000,
+  starName : '별',
+  treeName : '트리',
+  santaName : '산타',
+});
