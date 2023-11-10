adfd5d0eabb3bf668f6dd44c507dbc4d8624848e
coldok <2coldok@gmail.com>
2023-11-10 20:55:28 +0900

    feat : PREFIX 에러 메세지를 붙여주는 클래스 구현 및 에러메세지 상수화

---
+++ src/constants/ErrorMesseage.js
@@ -0,0 +1,12 @@
+export const ERROR_DATE = Object.freeze({
+  basic : '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+
+  
+
+});
+
+export const ERROR_MENU = Object.freeze({
+  basic : '유효하지 않은 주문입니다. 다시 입력해 주세요.',
+
+
+});
