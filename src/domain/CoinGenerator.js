7ff51b639c0605c474e7394484bb4d3007412e9f
coldok <2coldok@gmail.com>
2023-12-12 18:51:35 +0900

    feat : 동전 무작위 생성 기능 구현

---
+++ src/domain/CoinGenerator.js
@@ -0,0 +1,54 @@
+import { Random } from "@woowacourse/mission-utils";
+import { COIN } from "../constants/Constants.js";
+
+class CoinGenerator {
+  #coinMap;
+
+  #coinIndex = Array.from({ length : 4 }, (_, index) => index);
+  #inputMoney; 
+
+  constructor(inputMoney) {
+    this.#inputMoney = inputMoney;
+    this.#setCoinMap();
+    this.#setCoins();
+  }
+
+  #setCoinMap() {
+    this.#coinMap = new Map()
+    .set(10, 0)
+    .set(50, 0)
+    .set(100, 0)
+    .set(500, 0);
+  }
+
+  #setCoins() {
+    if (this.#inputMoney === 0) return;
+
+    const coin = this.#getRandomCoin();
+    const count = this.#coinMap.get(coin) + 1;
+    this.#coinMap.set(coin, count);
+    this.#inputMoney -= coin;
+
+    return this.#setCoins();
+  }
+
+  #getRandomCoin() {
+    const coin =  COIN[Random.pickNumberInList(this.#coinIndex)];
+    
+    if (this.#inputMoney < coin) return this.#getRandomCoin();
+    
+    return coin;
+  }
+
+  getCoinData() {
+    return Array.from(this.#coinMap);
+  }
+}
+
+export default CoinGenerator;
+
+const a = new CoinGenerator(450);
+
+const k = a.getCoinData();
+
+console.log(k);
