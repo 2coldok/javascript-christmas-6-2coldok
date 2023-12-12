a8b0e2331d7fb469eb2cb286f027fba4dbb38d43
coldok <2coldok@gmail.com>
2023-12-12 19:42:11 +0900

    feat : 모든 상품 수량이 0인지 판단하는 기능 구현

--- src/domain/VendingMachine.js
+++ src/domain/VendingMachine.js
@@ -16,9 +16,22 @@ class VendingMachine {
     this.#items = [];
     items.split(';').forEach((element) => this.#items.push(new Items(element)));
   }
-  // 
+  // 투입금액이 모든 상품 금액보다 적은가?
+  // 투입 금액시 검증
   isLowerMoneyForAllPrice() {
     const count = this.#items.filter((item) => item.getPrice() > this.#inputMoney);
+
+    if (count.length === this.#items.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //모든 상품 수량이 0인가?
+  //구매 상품명 입력시 검증
+  isAllItemsOring() {
+    const count = this.#items.filter((item) => item.getAmount() === 0);
 
     if (count.length === this.#items.length) {
       return true;
@@ -37,5 +50,5 @@ export default VendingMachine;
 const vendingMachine = new VendingMachine();
 vendingMachine.setInputMoney('999');
 vendingMachine.setItems('[콜라,1500,20];[사이다,1000,10]');
-console.log(vendingMachine.isLowerMoneyForAllPrice());
-
+/*console.log(vendingMachine.isLowerMoneyForAllPrice());*/
+/*console.log(vendingMachine.isAllItemsOring());*/
