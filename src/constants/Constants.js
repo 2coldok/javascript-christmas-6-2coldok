cb8eefa65cd4e197642a6403a50543e6812274e9
coldok <2coldok@gmail.com>
2023-12-13 19:30:50 +0900

    docs : 초안 작성

---
+++ src/constants/Constants.js
@@ -0,0 +1,8 @@
+const FRONTEND_CREW = '보노 시저 쉐리 신디 다비 덴버 이브 제시 라라 린다 리사 니콜 로드 윌터 제키'; // 6pair 1트리플 => 총 7 페어
+const BACKEND_CREW = '백호 태웅 치수 태섭 대만 준호 대협 덕규 태산 경태 수겸 현준 준섭 한나 소연 호열 대남 용팔 구식 달재'; // 10pair => 총 10 페어
+
+const map = new Map();
+map.set(1, 'hello');
+map.set(1, 'world');
+
+console.log(map);
