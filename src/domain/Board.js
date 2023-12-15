ca88d25ce2817878ccbea772ae8caa2dcd9aabd2
coldok <2coldok@gmail.com>
2023-12-15 14:01:38 +0900

    feat(Board) : 매칭 정보 조회 기능 구현

--- src/domain/Board.js
+++ src/domain/Board.js
@@ -1,4 +1,11 @@
 import { boardForm } from "../utils/Formatter.js";
+/* 사용법
+1. searchHasDataInBoard(data) 로 매칭 기록이 잇는지 확인 true 면 매칭 기록 잇는거임 
+매칭 저장전, 조회전 외부에서 이용하기
+2. 같은 레벨에 같은 페어 잇는지 확인
+3. record(data, pairs) 랜덤 pair 저장
+
+*/
 
 class Board {
   /*
@@ -17,19 +24,40 @@ class Board {
     this.#frontendBoard = new Map();
     this.#backendBoard = new Map();
   }
+
+  #courseBoard(course) {
+    if (course === '프론트엔드') return this.#frontendBoard;
+    if (course === '백엔드') return this.#backendBoard;
+  }
   
   // input1 : data ['프론트엔드', '레벨1', '자동차경주']
   // input2 : pairs
   record(data, pairs) {
+
     const [course, , ] = data;
     const {mission, levelPairs} = boardForm(data, pairs);
 
-    if (course === '프론트엔드') {
-      this.#frontendBoard.set(mission, levelPairs)
-    }
-    if (course === '백엔드') {
-      this.#backendBoard.set(mission, levelPairs);
-    }
+    this.#courseBoard(course).set(mission, levelPairs)
+  }
+  // 이미 매칭 기록이 있는지 확인. course, mission 으로 판단
+  // 있으면 true
+  // 없으면 false 
+  searchHasDataInBoard(data) {
+    const [course, _, mission] = data;
+    
+    if (this.#courseBoard(course).has(mission)) return true
+    
+    return false;
+  }
+  
+  // 페어 조회
+  // input : data ['프론트엔드', '레벨1', '자동차경주']
+  // output : pairs
+  getPairs(data) {
+    const [course, level, mission] = data;
+    const [_, pairs] = this.#courseBoard(course).get(mission);
+
+    return pairs;
   }
 
 
@@ -46,6 +74,7 @@ export default Board;
 const a = new Board();
 a.record(['프론트엔드', '레벨1', '자동차경주'], [['a', 'b'], ['c', 'd']]);
 a.record(['프론트엔드', '레벨1', '로또'], [[1,2], [3,4]]);
-a.record(['프론트엔드', '레벨1', '자동차경주'], [1,2])
-console.log(a.getMap());
+a.record(['프론트엔드', '레벨1', '숫자 야구'], [1,2])
+
+console.log(a.getPairs(['프론트엔드', '레벨1', '숫자 야구']));
 
